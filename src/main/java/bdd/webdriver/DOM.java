84b59258a07d632e8909ae0ed1e12902f873513a
Christian Andersson Garcia Reyes <34752076+christiangarciareyes@users.noreply.github.com>
2024-07-27 15:39:08 -0500

    Update DOM.java

--- src/main/java/bdd/webdriver/DOM.java
+++ src/main/java/bdd/webdriver/DOM.java
@@ -15,14 +15,16 @@ import java.io.IOException;
 public class DOM {
 
     // static String screenshotDir = "C:\\Users\\cgarcia\\Documents\\Producción TI\\DevOps\\Selenium\\Java\\selenium-cucumber-jenkins-main\\src\\test\\resources\\evidencias\\";
-    static String screenshotDir = "C:\\Users\\Usuario\\Documents\\Testing\\Evidencias\\";
-    
+    static String screenshotDir = "C:\\Users\\crist\\Documents\\Testing\\Evidencias\\"; //Server-1
+    // static String screenshotDir = "C:\\Users\\Usuario\\Documents\\Testing\\Evidencias\\"; //Server-2
+
     private static WebDriver driver;
 
     public static WebDriver webDriver() {
         if (driver == null) {
             // String chromeDriverPath = "C:\\Users\\cgarcia\\Documents\\Producción TI\\DevOps\\Selenium\\Java\\selenium-cucumber-jenkins-main\\src\\test\\resources\\webdriver\\chromedriver.exe";
-            String chromeDriverPath = "C:\\Users\\Usuario\\Documents\\Testing\\chromedriver.exe";
+            String chromeDriverPath = "C:\\Users\\crist\\Documents\\Testing\\chromedriver.exe"; //Server-1
+            // String chromeDriverPath = "C:\\Users\\Usuario\\Documents\\Testing\\chromedriver.exe"; //Server-2
             System.setProperty("webdriver.chrome.driver", chromeDriverPath);
             driver = new ChromeDriver();
         }
